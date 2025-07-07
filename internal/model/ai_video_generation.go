e9863495e463d57c87f0ea72fb370ea5ce5b0698
unknown <pinto9435@gmail.com>
2025-07-07 00:57:29 -0500

    Create Video Model for VideoGan API Payload

---
+++ internal/model/ai_video_generation.go
@@ -0,0 +1,44 @@
+package model
+
+type Background struct {
+	Type string `json:"type"`
+	URL  string `json:"url"`
+}
+
+type Voice struct {
+	Type      string  `json:"type"`
+	VoiceID   string  `json:"voice_id"`
+	InputText string  `json:"input_text"`
+	Speed     float32 `json:"speed"`
+}
+
+type Offset struct {
+	X float32 `json:"x"`
+	Y float32 `json:"y"`
+}
+
+type Character struct {
+	Type           string  `json:"type"`
+	TalkingPhotoID string  `json:"talking_photo_id"`
+	Offset         Offset  `json:"offset"`
+	Scale          float32 `json:"scale"`
+	TalkingStyle   string  `json:"talking_style"`
+	Expression     string  `json:"expression"`
+}
+
+type VideoInputs struct {
+	Character  Character  `json:"character"`
+	Voice      Voice      `json:"voice"`
+	Background Background `json:"background"`
+}
+
+type Dimension struct {
+	Width  int `json:"width"`
+	Height int `json:"height"`
+}
+
+type VideoData struct {
+	Caption     string      `json:"caption"`
+	Dimention   Dimension   `json:"dimension"`
+	VideoInputs VideoInputs `json:"video_inputs"`
+}
